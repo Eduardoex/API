65d068906008f513aeb986073313b016741e08b0
Eduardo de Lima <eduardodelimaex@gmail.com>
2023-04-16 09:25:55 -0300

    add index/userroutes

--- src/server.js
+++ src/server.js
@@ -1,18 +1,14 @@
 const express = require("express");
 
+const routes = require("./routes")
+
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 
+app.use(routes);
 
-
-app.post('/users', (request, response) => {
-  const {name, email, password} = request.body;
-
-  response.send({name, email, password});
-})
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
-
 

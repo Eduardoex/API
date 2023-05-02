1cef6f7ca9ffb27d4f7aca046e39304232dee186
Eduardo de Lima <eduardodelimaex@gmail.com>
2023-05-01 22:13:52 -0300

    add tagsController

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -68,7 +68,17 @@ async index (request, response){
    
     const filterTags = tags.split(',').map(tag => tag.trim())
     
-    notes = await knex("tags").whereIn("name", filterTags)
+    notes = await knex("tags")
+    .select([
+    "notes.id", 
+    "notes.title", 
+    "notes.user_id",
+  ])
+    .where("notes.user_id", user_id)
+    .whereLike("notes.title", `%${title}%`)
+    .whereIn("name", filterTags)
+    .innerJoin("notes", "notes.id", "tags.note_id")
+    .orderBy("notes.title")
     
     
 
@@ -79,10 +89,21 @@ async index (request, response){
   .orderBy("title");
   
 }
-return response.json(notes)
 
-}
+const userTags = await knex("tags").where({user_id})
+const notesWithTags = notes.map(note =>{
+  const noteTags = userTags.filter(tag => tag.note_id === note.id)
 
+  return {
+    ...note,
+    tags: noteTags
+  }
+
+})
+
+return response.json(notesWithTags)
+
+  }
 }
 
 

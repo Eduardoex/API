cf33b90c132ec82fc90d45bcd982547eaaea68dc
Eduardo de Lima <eduardodelimaex@gmail.com>
2023-06-29 21:31:00 -0300

    UPLOAD IMAGE

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 require("express-async-errors");
-
 const migrationsRun = require("./database/sqlite/migrations")
 const AppError = require ("./utils/AppError");
+const uploadConfig = require("./configs/upload")
 
 const express = require("express");
 
@@ -10,6 +10,7 @@ const routes = require("./routes")
 const app = express();
 app.use(express.json());
 
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
 
 app.use(routes);
 
